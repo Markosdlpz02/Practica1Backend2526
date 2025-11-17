5ccc9e68a3cf44d4032c7ccd5189a541f9bfb51e
Markosdlpz02 <markosdlopz@gmail.com>
2025-11-17 21:12:46 +0100

    API Tienda Online funcional

---
+++ P3/src/mongo.ts
@@ -0,0 +1,24 @@
+import { Db, MongoClient } from "mongodb";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+let client: MongoClient;
+let dB: Db;
+const dbName = "Backend2526";
+
+export const connectMongoDB = async (): Promise<void> => {
+  try {
+
+    const mongoUrl = `mongodb+srv://${process.env.USER_MONGO}:${process.env.USER_PASSWORD}@${process.env.MONGO_CLUSTER}.sftj0.mongodb.net/?appName=${process.env.MONGO_APP_NAME}`
+    
+    client = new MongoClient(mongoUrl);
+    await client.connect();
+    dB = client.db(dbName);
+    console.log("Connected to mongodb at db " + dbName);
+  } catch (error) {
+    console.log("Error mongo: ", error);
+  }
+};
+
+export const getDb = ():Db => dB;
---
+++ P3/src/types.ts
@@ -0,0 +1,35 @@
+import { ObjectId } from "mongodb";
+
+
+export type JwtPayload = {
+  id: string;
+  email: string;
+}
+
+export type User = {
+  _id?: ObjectId;
+  username: string;
+  email: string;
+  passwordHash: string;
+  createdAt: Date;
+}
+
+export type Product = {
+  _id?: ObjectId;
+  name: string;
+  description?: string; 
+  price: number;
+  stock: number;
+  createdAt: Date;
+}
+
+export type CartItem = {
+  productId: ObjectId;
+  quantity: number;
+}
+
+export type Cart = {
+  _id?: ObjectId;
+  userId: ObjectId; 
+  items: CartItem[];
+}
